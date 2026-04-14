7102eb95ab68150d94c6c14ec25d41cb323a0733
marta <mnaveira@minsait.com>
2026-04-14 20:53:25 +0200

    feat: add global loading interceptor and spinner overlay

--- frontend/src/app/core/interceptors/loader.interceptor.ts
+++ frontend/src/app/core/interceptors/loader.interceptor.ts
@@ -1,5 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { delay, finalize } from 'rxjs';
+
+import { LoadingService } from '../services/loading.service';
 
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const loadingService = inject(LoadingService);
+
+  loadingService.show();
+
+  return next(req).pipe(
+    delay(500),
+    finalize(() => loadingService.hide())
+  );
 };
--- frontend/src/app/core/layout/shell/shell.component.ts
+++ frontend/src/app/core/layout/shell/shell.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { RouterOutlet } from '@angular/router';
+import { LoadingSpinnerComponent } from "../../../shared/components/loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, LoadingSpinnerComponent],
   templateUrl: './shell.component.html',
   styleUrl: './shell.component.css'
 })
